0a30c1c17774563319753c3b6e5ad2e7b9ec0a01
aghabalaguluzade <aghabalaguluzade@gmail.com>
2024-09-21 17:04:45 +0400

    implement models and migrations

---
+++ backend/app/Models/Trip.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['is_started', 'is_complete', 'origin', 'destination', 'destination_name', 'driver_location'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class);
+    }
+}
