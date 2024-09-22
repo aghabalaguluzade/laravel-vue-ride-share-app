087bf91dcbf46cf7022f274f9391903a2d45d2f4
aghabalaguluzade <aghabalaguluzade@gmail.com>
2024-09-22 22:12:16 +0400

    Add trip controller methods for accepting, starting, and ending trips

--- backend/app/Models/Trip.php
+++ backend/app/Models/Trip.php
@@ -11,6 +11,16 @@ class Trip extends Model
 
     protected $fillable = ['is_started', 'is_complete', 'origin', 'destination', 'destination_name', 'driver_location'];
 
+    protected function casts(): array
+    {
+        return [
+            'origin' => 'array',
+            'destination' => 'array',
+            'is_started' => 'boolean',
+            'is_complete' => 'boolean'
+        ];
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
